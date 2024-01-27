a516424ea26cebc13066c5736c5e945a2920fb66
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-01-27 11:05:11 +0900

    comp-app

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import { Task } from "@/types";
+import Todo from "./Todo";
 
-const TodoList = () => {
+interface TodoListProps {
+  tasks: Task[];
+}
+
+export default function ToDoList({ tasks }: TodoListProps) {
   return (
     <ul className="space-y-3">
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>散歩</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500">Delete</button>
-        </div>
+      {/* <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+        <span className="text-gray-700">Task 1</span>
+        <button className="text-red-500 hover:text-red-700">Remove</button>
       </li>
       <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>買い物</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500">Delete</button>
-        </div>
-      </li>
-      <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
-        <span>バイト</span>
-        <div>
-          <button className="text-green-500 mr-3">edit</button>
-          <button className="text-red-500">Delete</button>
-        </div>
-      </li>
+        <span className="text-gray-700">Task 2</span>
+        <button className="text-red-500 hover:text-red-700">Remove</button>
+      </li> */}
+      {tasks.map((task) => (
+        // <li
+        //   key={task.id}
+        //   className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
+        // >
+        //   <span className="text-gray-700">{task.text}</span>
+        //   <button className="text-red-500 hover:text-red-700">✖</button>
+        // </li>
+        <Todo key={task.id} task={task} />
+      ))}
     </ul>
   );
-};
-
-export default TodoList;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import AddTask from './components/AddTask';
 import TodoList from './components/TodoList';
+import { getAllTodos } from '@/api';
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-200">
       <h1 className="text-4xl font-bold text-gray-700 -mt-32">
@@ -11,7 +14,7 @@ export default function Home() {
       <div className="w-full max-w-xl mt-5">
         <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
           <AddTask />
-          <TodoList />
+          <TodoList tasks={tasks} />
         </div>
       </div>
     </main>
